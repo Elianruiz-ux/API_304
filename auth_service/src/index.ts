84501ade16887fe1247f27c7e675fcdc69f15acc
Elian Joel Ruiz Callejas <elyan0811@gmail.com>
2023-03-25 07:51:27 -0500

    servicio #3

--- auth_service/src/index.ts
+++ auth_service/src/index.ts
@@ -1,9 +1,42 @@
-import express  from "express";
+import express, { json }  from "express";
 
 const server = express();
+server.use(json());
+
+const users: {[key: string | number]: any} ={};
 
 server.get('/api', (req, res) => {
-    res.send({message: 'hello'});
+    res.send(users);
+});
+
+server.get('/api/:id', (req, res) => {
+    res.send(users[req.params.id]);
+});
+
+server.post('/api', (req, res) => {
+    console.log(req.body, "Este es el body que enviamos desde la aplicacion");
+    const id = Math.ceil((Math.random() * 10000))
+    users[id] = {
+        id, 
+        ...req.body
+    }
+    res.send(users[id]);
+});
+
+server.patch('/api/:id', (req, res) => {
+    const {id} = req.params;
+    users[id] = {
+        id,
+        ...req.body
+    }
+
+    res.send(users);
+});
+
+server.delete('/api/:id', (req, res) => {
+    const {id} = req.params;
+    delete users[id];
+    res.send();
 });
 
 
